6c4f434b063803999f6a69c601767dddc057c15a
codev911 <codev911@gmail.com>
2024-10-04 18:33:14 +0700

    update config

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,14 @@ if [ ! -f /data/ipfs/config ]; then
   echo "Confiuring CORS IPFS..."
   ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
   ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+
   echo "Configuring Auto Relay IPFS..."
   ipfs config --json Swarm.EnableRelayHop false
   ipfs config --json Swarm.EnableAutoRelay true
+
+  # Konfigurasi IPFS untuk mengizinkan akses publik
+  ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 fi
 
 # Jalankan IPFS daemon setelah inisialisasi dan konfigurasi selesai
