bf033a5511c972a4b92804e9476f058827f43541
codev911 <codev911@gmail.com>
2024-10-04 13:18:28 +0700

    disable

--- scripts/configure_ipfs.sh
+++ scripts/configure_ipfs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# Pastikan IPFS diinisialisasi (jika belum ada)
-if [ ! -f /data/ipfs/config ]; then
-  ipfs init
-fi
+# # Pastikan IPFS diinisialisasi (jika belum ada)
+# if [ ! -f /data/ipfs/config ]; then
+#   ipfs init
+# fi
 
-# Memperbarui konfigurasi IPFS
-ipfs config --json Swarm.ConnMgr.LowWater 20
-ipfs config --json Swarm.ConnMgr.HighWater 40
-ipfs config Swarm.ConnMgr.GracePeriod "20s"
+# # Memperbarui konfigurasi IPFS
+# ipfs config --json Swarm.ConnMgr.LowWater 20
+# ipfs config --json Swarm.ConnMgr.HighWater 40
+# ipfs config Swarm.ConnMgr.GracePeriod "20s"
 
 # Konfigurasi CORS untuk API
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
@@ -18,8 +18,8 @@ ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]
 ipfs config --json Swarm.EnableAutoRelay true
 ipfs config --json Swarm.EnableRelayHop false
 
-# Menonaktifkan DHT (agar node tidak mengunduh dari node lain)
-ipfs config Routing.Type none
+# # Menonaktifkan DHT (agar node tidak mengunduh dari node lain)
+# ipfs config Routing.Type none
 
-# Menonaktifkan garbage collection sepenuhnya
-ipfs config Datastore.GCPeriod "-1"  # Menggunakan "-1" sebagai nilai
+# # Menonaktifkan garbage collection sepenuhnya
+# ipfs config Datastore.GCPeriod "-1"  # Menggunakan "-1" sebagai nilai
