dc56608f323cd263d299195e839fdceab708f2cd
codev911 <codev911@gmail.com>
2024-10-04 12:57:10 +0700

    update

--- scripts/configure_ipfs.sh
+++ scripts/configure_ipfs.sh
@@ -10,6 +10,10 @@ fi
 # ipfs config --json Swarm.ConnMgr.HighWater 40
 # ipfs config Swarm.ConnMgr.GracePeriod "20s"
 
+# Configurasi cors
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://192.168.33.101:5001", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+
 # Mengaktifkan AutoRelay dan menonaktifkan Relay Hop
 ipfs config --json Swarm.EnableAutoRelay true
 ipfs config --json Swarm.EnableRelayHop false
