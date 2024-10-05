fe98ecbfa48e61377b0dfe6c20a07f5a774adefd
codev911 <codev911@gmail.com>
2024-10-05 09:13:58 +0700

    edit entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# Cek apakah IPFS sudah diinisialisasi
 if [ ! -f /data/ipfs/config ]; then
   echo "Initializing IPFS..."
   ipfs init
 
-  # Terapkan konfigurasi hanya setelah inisialisasi
   echo "Confiuring CORS IPFS..."
   ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
   ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
@@ -14,11 +12,13 @@ if [ ! -f /data/ipfs/config ]; then
   ipfs config --json Swarm.EnableRelayHop false
   ipfs config --json Swarm.EnableAutoRelay true
 
-  # Konfigurasi IPFS untuk mengizinkan akses publik
+  echo "Configuring Gateway Prevent Fetch From Another Node"
+  ipfs config --bool Gateway.NoFetch true
+
+  echo "Configuring Global IP Address.."
   ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
   ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 fi
 
-# Jalankan IPFS daemon setelah inisialisasi dan konfigurasi selesai
 echo "Starting IPFS daemon..."
 ipfs daemon
